3cd338d5c20992c308ee16af67b0d0e30bda07e5
AdrianBrianoGarcia <adrian.brianogarcia@gmail.com>
2020-09-08 12:30:18 -0500

    Multiple property source files

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -2,9 +2,11 @@ package guru.springframework;
 
 import guru.springframework.controllers.MyController;
 import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class DiDemoApplication {
@@ -15,7 +17,9 @@ public class DiDemoApplication {
 		MyController controller = (MyController) ctx.getBean("myController");
 
 		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
-
 		System.out.println(fakeDataSource.getUser());
+
+		FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+		System.out.println(fakeJmsBroker.getJmsUsername());
 	}
 }
